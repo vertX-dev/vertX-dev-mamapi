b66b2e4b88fd6df1cbba6064a0b4120eec4729de
vertX-dev <danasolovej8@gmail.com>
2025-06-30 20:35:12 +0300

    Create MSIFvData.js

---
+++ MSIFvAPI/MSIFvData.js
@@ -0,0 +1,17 @@
+export const holdSens = 17
+
+/**
+ * @typedef {Object} ItemFunctionData
+ * @property {string[]} functions - Array of function names (strings) associated with the item.
+ * @property {string[]} skillNames - Array of skill names (strings) for display.
+ * @property {string} scoreboardObj - Scoreboard objective name (max 16 characters).
+ * @property {string[]} fontIcons - Array of icons or characters for UI display.
+ */
+
+/** 
+ * @type {Object.<string, ItemFunctionData>}
+ * Maps item typeId strings to their function/skill configuration.
+ */
+export const IFD = {
+    
+};
