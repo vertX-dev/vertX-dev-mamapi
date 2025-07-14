2830cb8a079208dac8de29de88924727de5d9733
vertX-dev <danasolovej8@gmail.com>
2025-07-14 21:49:27 +0300

    Update dataLib.js

--- ITEM RARITY/dataLib.js
+++ ITEM RARITY/dataLib.js
@@ -137,26 +137,125 @@ export const blackList = [
     "minecraft:apple"
 ];
 
-export const TagMapping = [
-    "sword",
-    "pickaxe",
-    "axe",
-    "shovel",
-    "hoe",
-    "mace",
+//16 -> 495
+const TagMapping = {
+    // Melee Weapons
+    "sword": ["sword", "katana", "claymore", "rapier", "shortsword", "longsword", "greatsword", "broadsword", "cutlass", "sabre", "scimitar", "falchion", "blade", "saber"],
+    "axe": ["axe", "battleaxe", "handaxe", "tomahawk", "great_axe", "lumber_axe", "hatchet", "cleaver"],
+    "pickaxe": ["pickaxe", "pick", "mattock", "paxel"],
+    "mace": ["mace", "club", "cudgel", "baton"],
+    "hammer": ["hammer", "warhammer", "maul", "sledgehammer", "jackhammer"],
+    "dagger": ["dagger", "knife", "blade", "stiletto", "dirk", "shiv"],
+    "spear": ["spear", "lance", "javelin", "pike", "halberd", "glaive", "polearm"],
+    "trident": ["trident", "fork", "pitchfork"],
+    "scythe": ["scythe", "sickle", "reaper"],
+    "staff": ["staff", "rod", "wand", "cane", "pole"],
+    "flail": ["flail", "morningstar", "chain"],
+    "whip": ["whip", "lash", "cord"],
+    "multitool": ["multitool", "omnitool", "swiss", "utility"],
 
-    "helmet",
-    "chestplate",
-    "leggings",
-    "boots",
+    // Tools
+    "shovel": ["shovel", "spade", "excavator"],
+    "hoe": ["hoe", "tiller", "cultivator"],
+    "drill": ["drill", "auger", "borer"],
+    "saw": ["saw", "chainsaw", "hacksaw", "bandsaw"],
+    "wrench": ["wrench", "spanner", "key"],
+    "chisel": ["chisel", "gouge", "carver"],
+    "file": ["file", "rasp", "grater"],
+    "crowbar": ["crowbar", "pry", "lever"],
+    "screwdriver": ["screwdriver", "driver", "turner"],
+    "pliers": ["pliers", "tongs", "pincers", "tweezers"],
+    "shears": ["shears", "scissors", "snips", "cutters"],
+    "fishing_rod": ["fishing", "rod", "pole", "line"],
+    "net": ["net", "mesh", "web", "snare"],
+    "bucket": ["bucket", "pail", "container"],
+    "flask": ["flask", "bottle", "vial", "phial"],
+    "torch": ["torch", "lantern", "lamp", "light"],
+    "compass": ["compass", "navigator", "direction"],
+    "rope": ["rope", "cord", "string", "twine"],
+    "hook": ["hook", "grappling", "anchor"],
+    "lockpick": ["lockpick", "pick", "jimmy"],
+    "key": ["key", "opener", "unlocker"],
+    "magnifying_glass": ["magnifying", "lens", "glass"],
+    "telescope": ["telescope", "scope", "viewer"],
+    "binoculars": ["binoculars", "field", "glasses"],
 
-    "bow",
-    "crossbow",
-    "trident",
-    "shield",
-    "elytra",
-    "totem"
-];
+    // Ranged Weapons
+    "bow": ["bow", "longbow", "shortbow", "composite", "recurve", "hunting"],
+    "crossbow": ["crossbow", "arbalest", "ballista"],
+    "sling": ["sling", "slingshot", "catapult"],
+    "blowgun": ["blowgun", "blowpipe", "dart"],
+    "throwing_knife": ["throwing", "kunai", "shuriken", "star"],
+    "boomerang": ["boomerang", "chakram", "disc"],
+    "gun": ["gun", "firearm", "weapon"],
+    "pistol": ["pistol", "handgun", "revolver", "sidearm"],
+    "rifle": ["rifle", "musket", "carbine", "firearm"],
+    "shotgun": ["shotgun", "scattergun", "blunderbuss"],
+    "sniper_rifle": ["sniper", "precision", "marksman"],
+    "assault_rifle": ["assault", "automatic", "machine"],
+    "launcher": ["launcher", "rocket", "grenade", "bazooka"],
+    "cannon": ["cannon", "artillery", "mortar"],
+
+    // Armor - Head
+    "helmet": ["helmet", "helm", "cap", "hat", "headgear", "casque", "bascinet", "sallet", "barbute", "armet"],
+    "crown": ["crown", "circlet", "tiara", "diadem", "coronet"],
+    "mask": ["mask", "visor", "face", "guard"],
+    "goggles": ["goggles", "glasses", "spectacles", "eyewear"],
+    "hood": ["hood", "cowl", "coif", "caul"],
+
+    // Armor - Chest
+    "chestplate": ["chestplate", "cuirass", "breastplate", "chest", "torso", "body"],
+    "vest": ["vest", "jacket", "coat", "tunic"],
+    "robe": ["robe", "gown", "dress", "garment"],
+    "chainmail": ["chainmail", "chain", "mail", "hauberk"],
+    "scale_mail": ["scale", "scaled", "lamellar"],
+    "plate_mail": ["plate", "plated", "full"],
+    "leather_armor": ["leather", "hide", "skin"],
+    "cloak": ["cloak", "cape", "mantle", "shroud"],
+
+    // Armor - Legs
+    "leggings": ["leggings", "pants", "trousers", "leg", "lower"],
+    "greaves": ["greaves", "shin", "leg", "guard"],
+    "tassets": ["tassets", "thigh", "upper"],
+
+    // Armor - Feet
+    "boots": ["boots", "shoes", "footwear", "foot", "sabatons"],
+    "sandals": ["sandals", "slippers", "footwear"],
+
+    // Armor - Hands/Arms
+    "gloves": ["gloves", "gauntlets", "mittens", "hand", "finger"],
+    "bracers": ["bracers", "vambraces", "arm", "forearm", "wrist"],
+
+    // Armor - Accessories
+    "shield": ["shield", "buckler", "targe", "aegis", "defender"],
+    "belt": ["belt", "girdle", "sash", "waist"],
+    "backpack": ["backpack", "pack", "bag", "satchel", "knapsack"],
+    "quiver": ["quiver", "arrow", "bolt", "case"],
+    "scabbard": ["scabbard", "sheath", "holster", "holder"],
+    "pouch": ["pouch", "purse", "wallet", "pocket"],
+
+    // Special/Utility Items
+    "elytra": ["elytra", "wings", "wing", "flight"],
+    "jetpack": ["jetpack", "thruster", "propulsion"],
+    "parachute": ["parachute", "chute", "glider"],
+    "totem": ["totem", "idol", "fetish", "charm"],
+    "amulet": ["amulet", "talisman", "pendant", "necklace"],
+    "ring": ["ring", "band", "circle"],
+    "bracelet": ["bracelet", "bangle", "armband"],
+    "earring": ["earring", "ear", "stud"],
+    "brooch": ["brooch", "pin", "badge", "insignia"],
+
+    // Magic Items
+    "wand": ["wand", "rod", "staff", "scepter", "baton"],
+    "orb": ["orb", "sphere", "crystal", "ball"],
+    "gem": ["gem", "jewel", "stone", "crystal"],
+    "focus": ["focus", "catalyst", "conduit", "channeler"],
+    "tome": ["tome", "book", "grimoire", "manual"],
+    "scroll": ["scroll", "parchment", "paper", "document"],
+    "rune": ["rune", "glyph", "symbol", "sigil"],
+    "artifact": ["artifact", "relic", "treasure", "ancient"],
+    "legendary": ["legendary", "epic", "rare", "unique", "mythic", "divine"]
+};
 
 export const stats = {
     // DAMAGE - All items
@@ -237,16 +336,16 @@ export const skills = {
     SMASH_LEAP_MYTHIC: { name: "§cSmash Leap", type: "mace", rarity: "Mythic", min: 5, max: 7, description: "§7Smashes the ground and stuns \n§7enemies in §x{x} §7blocks radius, \n§7then leaps 8 blocks if has wind charge", cooldown: 120, scoreboard: "smashleap" },
 
     // SPIN STRIKE - Sword
-    SPIN_STRIKE_RARE: { name: "§9Spin Strike", type: "sword", rarity: "Rare", min: 8, max: 12, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
-    SPIN_STRIKE_EPIC: { name: "§5Spin Strike", type: "sword", rarity: "Epic", min: 12, max: 18, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
-    SPIN_STRIKE_LEGENDARY: { name: "§6Spin Strike", type: "sword", rarity: "Legendary", min: 18, max: 25, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
-    SPIN_STRIKE_MYTHIC: { name: "§cSpin Strike", type: "sword", rarity: "Mythic", min: 25, max: 35, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
+    SPIN_STRIKE_RARE: { name: "§9Spin Strike", type: "sword", rarity: "Rare", min: 5, max: 6, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
+    SPIN_STRIKE_EPIC: { name: "§5Spin Strike", type: "sword", rarity: "Epic", min: 7, max: 9, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
+    SPIN_STRIKE_LEGENDARY: { name: "§6Spin Strike", type: "sword", rarity: "Legendary", min: 10, max: 15, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
+    SPIN_STRIKE_MYTHIC: { name: "§cSpin Strike", type: "sword", rarity: "Mythic", min: 15, max: 20, description: "§7Performs a 360° spin attack, \n§7dealing §x{x} §7damage to all \n§7enemies within 3 blocks", cooldown: 60, scoreboard: "spinstrike" },
 
     // EXPLOSIVE MINING - Pickaxe
     EXPLOSIVE_MINING_RARE: { name: "§9Explosive Mining", type: "pickaxe", rarity: "Rare", min: 2, max: 3, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
-    EXPLOSIVE_MINING_EPIC: { name: "§5Explosive Mining", type: "pickaxe", rarity: "Epic", min: 3, max: 4, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
-    EXPLOSIVE_MINING_LEGENDARY: { name: "§6Explosive Mining", type: "pickaxe", rarity: "Legendary", min: 4, max: 5, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
-    EXPLOSIVE_MINING_MYTHIC: { name: "§cExplosive Mining", type: "pickaxe", rarity: "Mythic", min: 5, max: 7, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
+    EXPLOSIVE_MINING_EPIC: { name: "§5Explosive Mining", type: "pickaxe", rarity: "Epic", min: 4, max: 5, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
+    EXPLOSIVE_MINING_LEGENDARY: { name: "§6Explosive Mining", type: "pickaxe", rarity: "Legendary", min: 6, max: 7, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
+    EXPLOSIVE_MINING_MYTHIC: { name: "§cExplosive Mining", type: "pickaxe", rarity: "Mythic", min: 8, max: 10, description: "§7Creates an explosion with \n§7size of §x{x} §7to break \n§7blocks and damage enemies", cooldown: 1000, scoreboard: "explosivemining" },
 
     // RAY MINER - Pickaxe
     RAY_MINER_RARE: { name: "§9Ray Miner", type: "pickaxe", rarity: "Rare", min: 8, max: 12, description: "§7Breaks §x{x} §7blocks in a \n§7straight line through solid \n§7materials", cooldown: 180, scoreboard: "rayminer" },
@@ -255,10 +354,10 @@ export const skills = {
     RAY_MINER_MYTHIC: { name: "§cRay Miner", type: "pickaxe", rarity: "Mythic", min: 20, max: 25, description: "§7Breaks §x{x} §7blocks in a \n§7straight line through solid \n§7materials", cooldown: 180, scoreboard: "rayminer" },
 
     // EXCAVATOR - Shovel
-    EXCAVATOR_RARE: { name: "§9Excavator", type: "shovel", rarity: "Rare", min: 3, max: 5, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
-    EXCAVATOR_EPIC: { name: "§5Excavator", type: "shovel", rarity: "Epic", min: 5, max: 7, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
-    EXCAVATOR_LEGENDARY: { name: "§6Excavator", type: "shovel", rarity: "Legendary", min: 7, max: 10, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
-    EXCAVATOR_MYTHIC: { name: "§cExcavator", type: "shovel", rarity: "Mythic", min: 10, max: 15, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
+    EXCAVATOR_RARE: { name: "§9Excavator", type: "shovel", rarity: "Rare", min: 2, max: 4, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
+    EXCAVATOR_EPIC: { name: "§5Excavator", type: "shovel", rarity: "Epic", min: 4, max: 6, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
+    EXCAVATOR_LEGENDARY: { name: "§6Excavator", type: "shovel", rarity: "Legendary", min: 6, max: 8, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
+    EXCAVATOR_MYTHIC: { name: "§cExcavator", type: "shovel", rarity: "Mythic", min: 7, max: 12, description: "§7Breaks sand-type blocks in a \n§73x3x§x{x} §7area, consuming \n§7durability", cooldown: 70, scoreboard: "excavator" },
 
     // FLAME ARC - Sword
     FLAME_ARC_RARE: { name: "§9Flame Arc", type: "sword", rarity: "Rare", min: 15, max: 20, description: "§7Unleashes an arc of fire that \n§7ignites enemies and blocks, \n§7dealing §x{x} §7fire damage", cooldown: 400, scoreboard: "flamearc" },
@@ -267,16 +366,16 @@ export const skills = {
     FLAME_ARC_MYTHIC: { name: "§cFlame Arc", type: "sword", rarity: "Mythic", min: 38, max: 50, description: "§7Unleashes an arc of fire that \n§7ignites enemies and blocks, \n§7dealing §x{x} §7fire damage", cooldown: 400, scoreboard: "flamearc" },
 
     // SHADOW DASH - Sword
-    SHADOW_DASH_RARE: { name: "§9Shadow Dash", type: "sword", rarity: "Rare", min: 6, max: 8, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
-    SHADOW_DASH_EPIC: { name: "§5Shadow Dash", type: "sword", rarity: "Epic", min: 8, max: 12, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
-    SHADOW_DASH_LEGENDARY: { name: "§6Shadow Dash", type: "sword", rarity: "Legendary", min: 12, max: 16, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
-    SHADOW_DASH_MYTHIC: { name: "§cShadow Dash", type: "sword", rarity: "Mythic", min: 16, max: 20, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
+    SHADOW_DASH_RARE: { name: "§9Shadow Dash", type: "sword", rarity: "Rare", min: 3, max: 5, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
+    SHADOW_DASH_EPIC: { name: "§5Shadow Dash", type: "sword", rarity: "Epic", min: 6, max: 8, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
+    SHADOW_DASH_LEGENDARY: { name: "§6Shadow Dash", type: "sword", rarity: "Legendary", min: 9, max: 12, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
+    SHADOW_DASH_MYTHIC: { name: "§cShadow Dash", type: "sword", rarity: "Mythic", min: 13, max: 15, description: "§7Dash forward §x{x} §7blocks \n§7through enemies, reduced \n§7distance when airborne", cooldown: 50, scoreboard: "shadowdash" },
 
     // VOID PIERCE - Sword
-    VOID_PIERCE_RARE: { name: "§9Void Pierce", type: "sword", rarity: "Rare", min: 12, max: 18, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
-    VOID_PIERCE_EPIC: { name: "§5Void Pierce", type: "sword", rarity: "Epic", min: 18, max: 25, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
-    VOID_PIERCE_LEGENDARY: { name: "§6Void Pierce", type: "sword", rarity: "Legendary", min: 25, max: 35, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
-    VOID_PIERCE_MYTHIC: { name: "§cVoid Pierce", type: "sword", rarity: "Mythic", min: 35, max: 50, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" }
+    VOID_PIERCE_RARE: { name: "§9Void Pierce", type: "sword", rarity: "Rare", min: 3, max: 5, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
+    VOID_PIERCE_EPIC: { name: "§5Void Pierce", type: "sword", rarity: "Epic", min: 6, max: 8, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
+    VOID_PIERCE_LEGENDARY: { name: "§6Void Pierce", type: "sword", rarity: "Legendary", min: 9, max: 14, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" },
+    VOID_PIERCE_MYTHIC: { name: "§cVoid Pierce", type: "sword", rarity: "Mythic", min: 15, max: 25, description: "§7Shoots a void projectile that \n§7pierces through enemies, \n§7dealing §x{x} §7damage", cooldown: 80, scoreboard: "voidpierce" }
 };
 
 export const passives = {
