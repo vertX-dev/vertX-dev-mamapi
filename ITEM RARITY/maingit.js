31b329c69fc96d4d996f3a644199eae376b5968c
vertX-dev <danasolovej8@gmail.com>
2025-07-18 01:24:14 +0300

    update ITEM RARITY/maingit.js

--- ITEM RARITY/maingit.js
+++ ITEM RARITY/maingit.js
@@ -723,7 +723,7 @@ function rarityItemTest(itemStack, player, rarityUp = "None", upGuarant = false)
                 // Upgrade to specific rarity
                 let rarity = Object.values(RARITY).find(r => r.sid === rarityUp);
                 if (!upGuarant) {
-                    const id = Math.min(rarity.id + Math.floor((Math.random() * 2) - 1), 6);
+                    const id = Math.max(Math.min(rarity.id + Math.floor((Math.random() * 2) - 1), 6));
                     
                 }
                 
