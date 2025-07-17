71931ee88703fe6e4a545dfa7c86b5d1ccefd7a4
vertX-dev <danasolovej8@gmail.com>
2025-07-17 12:55:09 +0300

    update ITEM RARITY/passivesLib.js

--- ITEM RARITY/passivesLib.js
+++ ITEM RARITY/passivesLib.js
@@ -13,17 +13,19 @@ export const passives = {
         type: meleeWeapons,
         rarity: "Rare",
         min: 1,
-        max: 1,
-        description: "slow enemy for §x{x} seconds on hit",
-        cooldown: 10,
+        max: 2,
+        description: "§7Slow enemy for §x{x} \n§7seconds on hit",
+        cooldown: 50,
         scoreboard: "frosttouch"
     },
     FROST_TOUCH_EPIC: {
         name: "§5Frost Touch",
         type: meleeWeapons,
         rarity: "Epic",
-        min: 2,
-        max: 2,
-        description: "slow enemy for §x{x} seconds on hit"
+        min: 1,
+        max: 3,
+        description: "§7Slow enemy for §x{x} \n§7seconds on hit",
+        cooldown: 30,
+        scoreboard: "frosttouch"
     }
 };
