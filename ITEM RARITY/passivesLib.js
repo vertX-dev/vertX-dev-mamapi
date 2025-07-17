9739b7af5c08c311ae98d69a197e4ab02c8e5c68
vertX-dev <danasolovej8@gmail.com>
2025-07-17 12:48:31 +0300

    update ITEM RARITY/passivesLib.js

--- ITEM RARITY/passivesLib.js
+++ ITEM RARITY/passivesLib.js
@@ -1,12 +1,29 @@
+import { 
+    allWeapons, 
+    allArmor, 
+    meleeWeapons,
+    rangedWeapons,
+    tools,
+    allItems
+} from './mainLib.js';
+
 export const passives = {
-    FROST_TOUCH: {
-        name: "§bFrost Touch",
-        type: "sword",
+    FROST_TOUCH_RARE: {
+        name: "§1Frost Touch",
+        type: meleeWeapons,
         rarity: "Rare",
         min: 1,
-        max: 2,
+        max: 1,
         description: "slow enemy for §x{x} seconds on hit",
         cooldown: 10,
         scoreboard: "frosttouch"
+    },
+    FROST_TOUCH_EPIC: {
+        name: "§5Frost Touch",
+        type: meleeWeapons,
+        rarity: "Epic",
+        min: 2,
+        max: 2,
+        description: "slow enemy for §x{x} seconds on hit"
     }
 };
