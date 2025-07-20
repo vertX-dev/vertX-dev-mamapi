16568f2d2285ed5b63de6e4d2815255d6817a123
vertX-dev <danasolovej8@gmail.com>
2025-07-20 11:10:49 +0300

    update Testenv.js

--- Testenv.js
+++ Testenv.js
@@ -1,16 +1,2 @@
-async function setup() {
-    console.log("load");
-    let pyodide = await loadPyodideAndPackages();
-    // Now the requests library is available for use
-    console.log("install");
-    let code = `
-        import requests
-
-        response = requests.get('https://api.github.com')
-        print(response.json())
-    `;
-    pyodide.runPython(code);
-    console.log("finish");
-}
-
-setup();
+let x = Math.floor(49.636782927 % 20);
+console.log(x);
