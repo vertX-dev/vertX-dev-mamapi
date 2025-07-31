2dc7f6c86914f9ff75226ab99b5f246d7230d8fe
vertX-dev <danasolovej8@gmail.com>
2025-07-31 21:38:51 +0300

    update Testenv.js

--- Testenv.js
+++ Testenv.js
@@ -1,20 +1,28 @@
-const fontCharacters = [
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','','',
-  '','','','','','','',''
-]; //81 real
-let ix = 40;
+const original = [
+  "§7Item info",
+  "§8Attributes",
+  "§cOld line 1",
+  "§cOld line 2",
+  "§a§t§b§e§n§d§r",
+  "§7Other info"
+];
 
-let result = fontCharacters[80 - ix];
+const insertContent = [
+  "§aNew stat 1",
+  "§aNew stat 2"
+];
 
+// Find the range
+const startIndex = original.indexOf("§8Attributes") + 1;
+const endIndex = original.indexOf("§a§t§b§e§n§d§r");
 
-console.log(result);
+// Replace the content in between
+const updated = [
+  ...original.slice(0, startIndex),
+  ...insertContent,
+  ...original.slice(endIndex)
+];
+
+for (const a of updated) {
+    console.log(a);
+}
