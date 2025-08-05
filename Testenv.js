063f0b8751f5670c7c4f307c1bdfad888de920ac
vertX-dev <danasolovej8@gmail.com>
2025-08-06 00:14:57 +0300

    update Testenv.js

--- Testenv.js
+++ Testenv.js
@@ -1,4 +1,4 @@
-import { world, EquipmentSlot, system } from "@minecraft/setver";
+import { world, EquipmentSlot, system, ItemStack } from "@minecraft/setver";
 
 const ITEMS = [
     "minecraft:apple"
@@ -6,6 +6,14 @@ const ITEMS = [
 
 function getRandomItem() {
     return ITEMS[Math.floor(Math.random() * ITEMS.length)];
+}
+
+function giveItem(player, item, container = player.getComponent("minecraft:inventory").container) {
+    if (container.emptySlotsCount > 0) {
+        container.addItem(item);
+    } else {
+        system.runTimeout(() => giveItem(player, item, container), 100);
+    }
 }
 
 world.afterEvents.playerSpawn.subscribe((ev) => {
