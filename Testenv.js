531af850570cce9f8a0ad4a1b170808bdd22ac44
vertX-dev <danasolovej8@gmail.com>
2025-08-06 00:09:00 +0300

    update Testenv.js

--- Testenv.js
+++ Testenv.js
@@ -1,5 +1,13 @@
 import { world, EquipmentSlot, system } from "@minecraft/setver";
 
+const ITEMS = [
+    "minecraft:apple"
+];
+
+function getRandomItem() {
+    return ITEMS[Math.floor(Math.random() * ITEMS.length)];
+}
+
 world.afterEvents.playerSpawn.subscribe((ev) => {
     
 });
