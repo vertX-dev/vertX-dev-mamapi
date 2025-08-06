6e2b227a9524e630ea566be421abe19117639bce
vertX-dev <danasolovej8@gmail.com>
2025-08-06 09:23:38 +0300

    update Testenv.js

--- Testenv.js
+++ Testenv.js
@@ -13,14 +13,16 @@ function getRandomItem() {
 }
 
 function giveItem(player, items, container = player.getComponent("minecraft:inventory").container) {
+    let fullItems = [];
     for (const item of items) {
         if (container.emptySlotsCount > 0) {
             container.addItem(item);
         } else {
             player.runCommand("tell @s clear inventory to get " + item.nameTag);
-            system.runTimeout(() => giveItem(player, item, container), 200);
+            fullItems.push(item);
         }
     }
+    if (fullItems.length > 0) system.runTimeout(() => giveItem(player, fullItems, container), 200);
 }
 
 function createItem(itemId) {
